45051687baa5ff65cc1115375ee2c0cac1144784
Rolo <robert.szabo@coxautoinc.com>
2020-08-24 19:03:23 -0500

    Adding a library i.e. package for the sake of seeing how to do it.~

--- simple-tp-calculator/calc_package/lib/calculator.dart
+++ simple-tp-calculator/calc_package/lib/calculator.dart
@@ -1,5 +1,8 @@
+library calc_package;
+
 import 'package:intl/intl.dart';
 
+/// Calculator responsible for determining TP longevity.
 class Calculator {
   int tprs = 0;
   int spr = 0;
@@ -10,10 +13,12 @@ class Calculator {
 
   Calculator();
 
+  /// Set all four values [tprs], [spr], [avgSps], and [burts] to calculate a days supply value.
   double calculate() {
     return tprs * spr / avgSps / burts;
   }
 
+  /// Convert the [calculate()] value to a readable string.
   String calculateAsString() {
     double value = calculate();
     return numberFormat.format(value);
--- simple-tp-calculator/lib/main.dart
+++ simple-tp-calculator/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app/Calculator.dart';
+import 'package:calc_package/calculator.dart';
 import 'package:flutter/material.dart';
 
 import 'CustomInputField.dart';
